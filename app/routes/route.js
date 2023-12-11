8b9014fd006ec91f2f5ec694b1fd43fab26ddc38
koo <kookh0987@gmail.com>
2023-12-11 17:22:07 +0900

    test : deploy

--- app/routes/route.js
+++ app/routes/route.js
@@ -5,7 +5,7 @@ const moment = require("moment");
 
 //테스트
 router.get("/", (req, res) => {
-  return res.status(200).send(process.env.NODE_ENV + " : working! 12-11-2");
+  return res.status(200).send(process.env.NODE_ENV + " : working! 12-11-3");
 });
 
 //slack 테스트
